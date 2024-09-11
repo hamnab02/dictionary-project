064f27f9a2d843d6bee9469f05528ed17d639bb2
Hamnab02 <hamnab02@gmail.com>
2024-09-11 16:42:38 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <h1>Dictionary App</h1>
+     <footer>This project was coded by Hamna Babar</footer>
     </div>
   );
 }
