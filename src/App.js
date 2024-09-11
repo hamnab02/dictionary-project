bcc9d3360fd788510d8eff97c68c1ee762ca36cf
Hamnab02 <hamnab02@gmail.com>
2024-09-11 19:14:35 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-     <h1>Dictionary App</h1>
-     <footer>This project was coded by Hamna Babar</footer>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary defaultKeyword="sunset" />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Hamna Babar</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
